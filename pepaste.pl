cbb4b64bc0cda3494a8fc5d1277cf67aa6173048
Radoslaw Jablonski <radoslaw.jablonski@hotmail.com>
2018-05-29 10:02:24 +0200

    Add friendly help information with param descriptions

--- pepaste.pl
+++ pepaste.pl
@@ -3,26 +3,31 @@
 use 5.14.0; # for 'say'
 use strict;
 use Getopt::Long qw(:config bundling); # for case sensitive
+use Pod::Usage qw(pod2usage);;
 
 my %params = ('num-words' => 2,
 			  'split-delim' => ' ',
 			  'match-word-regex' => '',
 			  'match-line-regex' => '',
-			  'verbose' => '0',
+			  'verbose' => 0,
 			  'end-line-prefix' => '',
-			  'output-word-separator' => ' ');
+			  'output-word-separator' => ' ',
+			  'help' => 0);
 
 GetOptions('num-words|n=i' => \$params{'num-words'},
 		   'split-delim|d=s' => \$params{'split-delim'},
 		   'match-word|m=s' =>  \$params{'match-word-regex'},
-		   'M=s' => \$params{'match-line-regex'},
+		   'match-line-regex|M=s' => \$params{'match-line-regex'},
 		   'verbose|v' => \$params{'verbose'},
 		   'end-line-prefix|e=s' => \$params{'end-line-prefix'},
-		   'output-word-separator|o=s' => \$params{'output-word-separator'}
+		   'output-word-separator|o=s' => \$params{'output-word-separator'},
+		   'help|h' => \$params{'help'}
 	   )
-	or die 'Invalid parameters';
+	or pod2usage(-verbose => 0);
+;
 
-#TODO: help info param msg
+## help info
+
 
 sub say_d {
 	# printing all params if debug is enabled
@@ -61,6 +66,10 @@ sub validate_regex {
 
 	say_d "Regex $regex validation ok";
 }
+
+
+### START ####
+pod2usage(0) if $params{'help'};
 
 say_d 'Using column size: '.$params{'num-words'};
 say_d 'Using word regex: '.$params{'match-word-regex'};
@@ -133,3 +142,55 @@ while (my $line = <>) {
 # flushing on the end - it causes problems on some shells
 # if not flushed..
 flush_if_needed;
+
+=head1 SYNOPSIS
+
+$ pepaste [-vh ] [ --num-words|-n NUM ]
+[ --split-delim|-d ' ' ]
+[ --match-word|-m '/match/' ]
+[ --match-line-regex|-M '/match/' ]
+[ --end-line-prefix|-e '' ]
+[ --output-word-separator|-w ' ' ]
+input_file
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--num-words N> or B<-n N>
+
+number of words per line that will be generated in output stream.
+
+=item B<--split-delim ' '> or B<-d ' '>
+
+delimiter of words in line in INPUT stream - used when each input line consists of more than one field
+
+=item B<--match-word '/match/'> or B<-m '/match/'>
+
+print only WORDS that matching given regex e.g.: -m '/a/' will print only words containing 'a' and rest will be filtered out
+
+=item B<--match-line-regex '/match/'> or B<-M '/match/'>
+
+print only LINES that matching given regex e.g.: -m '/^a/' will print only lines starting with 'a' and rest of the input lines be filtered out
+
+=item B<--end-line-prefix ''> or B<-e ''>
+
+set separator that is printed on end of every line just before newline marker.e.g: -e '\' will result in:
+a,b,c\
+d,e,f
+
+=item B<--output-word-separator ' '> or B<-w ' '>
+
+set separator that is used after every printed word. At default one whitespace is used (' '). e.g: -o ',' will result in:
+a,b,c
+d,e,f
+
+=item B<--verbose> or B<-v>
+
+prints additional debug information
+
+=item B<--help> or B<-h>
+
+this help information
+
+=back

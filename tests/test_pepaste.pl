765062698be1e7a976df450bdb6f16a13e05c0eb
Radoslaw Jablonski <radoslaw.jablonski@hotmail.com>
2018-06-08 21:21:28 +0200

    tests: Add empty input tests

--- tests/test_pepaste.pl
+++ tests/test_pepaste.pl
@@ -37,6 +37,13 @@ sub three_words_all_modes_test {
 	three_words_in_sep_lines_test($pepaste_params, $expected_str);
 }
 
+sub empty_input_test {
+	my $pepaste_params = shift;
+	my $expected_str = shift;
+
+	basic_test("/bin/echo", $pepaste_params, $expected_str, "Empty input");
+}
+
 # Three words in line, no params (default n=1)
 my $expected = "aa
 bb
@@ -49,6 +56,12 @@ $expected = "aa bb
 cc
 ";
 three_words_all_modes_test("-n 2", $expected);
+
+# Empty inputs tests, nothing should be generated
+$expected = "";
+empty_input_test("", $expected);
+empty_input_test("-n 3", $expected);
+empty_input_test("-e '\\'", $expected);
 
 
 done_testing();

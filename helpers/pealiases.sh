32a7b73662f3dee7efd23e67aba1787b10e80d60
Radoslaw Jablonski <radoslaw.jablonski@hotmail.com>
2019-01-20 21:11:01 +0100

    Escaped commands comments

--- helpers/pealiases.sh
+++ helpers/pealiases.sh
@@ -4,4 +4,7 @@ alias 'dkps'='sudo docker ps -a'
 alias 'dkls'="sudo docker images|pepaste -c '1,2' -s ':'"
 alias 'dkrm_all_running'='sudo docker rm -f `sudo docker ps|omit-header|pepaste -c 1`'
 alias 'dkrm_empty_im'='sudo docker images|perl -ane '\''print "$F[2] " if $F[0] eq "<none>"'\''|xargs sudo docker rmi'
+
+# omit one line
+# perl -ne 'print if $. > 1'
 alias 'omit-header'='perl -ne '\''print if $. > 1'\''   '

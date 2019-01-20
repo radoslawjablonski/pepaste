a3dec4c43542ee26d732b1cc2ca90b23467ac148
Radoslaw Jablonski <radoslaw.jablonski@hotmail.com>
2019-01-20 20:45:15 +0100

    Add alias for omit-header

--- helpers/pealiases.sh
+++ helpers/pealiases.sh
@@ -2,5 +2,6 @@ alias 'dk'='sudo docker'
 alias 'dkim'='sudo docker images'
 alias 'dkps'='sudo docker ps -a'
 alias 'dkls'="sudo docker images|pepaste -c '1,2' -s ':'"
-alias 'dkrm_all_running'='sudo docker rm -f `sudo docker ps|pepaste -c 1 -W CONTAIN`'
+alias 'dkrm_all_running'='sudo docker rm -f `sudo docker ps|omit-header|pepaste -c 1`'
 alias 'dkrm_empty_im'='sudo docker images|perl -ane '\''print "$F[2] " if $F[0] eq "<none>"'\''|xargs sudo docker rmi'
+alias 'omit-header'='perl -ne '\''print if $. > 1'\''   '
